0953c806a048541eaeff760010b5c9ac1dffdd05
Jake Saylor <jakesaylor4@gmail.com>
2020-04-07 16:34:12 -0600

    Card, Details, Run_Script Filter, Show More Button

--- campaignanalytics/src/home.js
+++ campaignanalytics/src/home.js
@@ -8,19 +8,24 @@ import ProductCard from './productCard';
 
 export default function Home(props) {
 
-    const context = React.useContext(AppContext);
-
+    const context = React.useContext(AppContext)
+    const numOfCamp =  context.showCount
+    console.log("This is num of Camp: " + numOfCamp)
     return (
         <bs.Container fluid className="text-center">
             <h3 className="mt-2">Welcome to Arctic Retailer</h3>
             <h6><i>"Our Products Will Put You on Top of the World"</i></h6>
             <bs.Row>
-                {context.campaign.map((prod) => {
+                {/* {numOfCamp.map((prod) => { */}
+                {context.campaign.slice(0, numOfCamp).map((prod) => {
                 return (
                     <ProductCard camp={prod} key={prod.campaign_id}/>
                 )
                 })}
             </bs.Row>
+         <bs.Button variant='warning' onClick= {e=>{
+                    context.loadMoreCamp(true);
+                }}>Show More Campaigns</bs.Button>
         </bs.Container>
 )
 }
