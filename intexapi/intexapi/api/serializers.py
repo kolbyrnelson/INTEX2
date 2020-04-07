d86ec84d6c3f2bd2ac10e292de8cd02313bb2443
Kolby Nelson <56325711+kolbyrnelson@users.noreply.github.com>
2020-04-06 22:08:32 -0600

    Database loaded

--- intexapi/intexapi/api/serializers.py
+++ intexapi/intexapi/api/serializers.py
@@ -6,7 +6,7 @@ from api.models import Campaign, Donation, Update
 class CampaignSerializer(serializers.ModelSerializer):
     class Meta:
         model = Campaign
-        fields = [ 'id', 'url', 'campaign_id', 'auto_fb_post_mode', 'collected_date', 'category_id',
+        fields = [ 'url', 'campaign_id', 'auto_fb_post_mode', 'collected_date', 'category_id',
          'category', 'currencycode', 'current_amount', 'goal', 'donators', 'days_active', 'days_created',
          'title', 'description', 'default_url', 'has_beneficiary', 'media_type', 'project_type' ,'turn_off_donations' ,
          'user_id', 'user_first_name', 'user_last_name', 'user_facebook_id', 'user_profile_url', 'visible_in_search',
@@ -17,13 +17,11 @@ class CampaignSerializer(serializers.ModelSerializer):
 class DonationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Donation
-        fields = [ 'id', 'update_id', 'campaign_id', 'collected_date', 'amount', 'is_offline', 'is_anonymous',
+        fields = [ 'donation_id', 'campaign_id', 'collected_date', 'amount', 'is_offline', 'is_anonymous',
         'name', 'created_at', 'profile_url', 'verified']
-        # fields = [ 'id', 'filename', 'name', 'description', 'price', 'category' ]
 
 class UpdateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Update
-        fields = [ 'id', 'update_id', 'campaign_id', 'collected_date', 'photo_url', 'created_at',
+        fields = [ 'update_id', 'campaign_id', 'collected_date', 'photo_url', 'created_at',
         'updates_author', 'updates_author_type', 'updates_text', 'comments']
-        # fields = [ 'name', 'address1', 'address2', 'city', 'state', 'zipcode', 'total', 'items', 'payment_intent' ]
