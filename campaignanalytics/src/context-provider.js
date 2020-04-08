a5e9a84ea069e62edfdd7b0b2e7fd2fe94365d4f
Kolby Nelson <56325711+kolbyrnelson@users.noreply.github.com>
2020-04-08 17:02:15 -0600

    Add more filters

--- campaignanalytics/src/context-provider.js
+++ campaignanalytics/src/context-provider.js
@@ -51,33 +51,45 @@ export default class AppProvider extends React.Component {
         }))
     }
 
-    filterCampaigns = (goalFilter, countryFilter, stateFilter) => {
+    filterCampaigns = (goalFilter, countryFilter, stateFilter, hasBeneficiaryFilter, percentGoalFilter) => {
         this.setState(state => produce(state, draft => {
+            // FILTER GOALS
             if(goalFilter === "<2000"){
-                draft.filteredCampaigns = draft.campaign.filter(camp => parseInt(camp.goal) < 2000);
-            }
+                draft.filteredCampaigns = draft.campaign.filter(camp => parseInt(camp.goal) < 2000)}
             else if(goalFilter === "2000-9999"){
-                draft.filteredCampaigns = draft.campaign.filter(camp => parseInt(camp.goal) >= 2000 && parseInt(camp.goal) < 10000);
-            }
+                draft.filteredCampaigns = draft.campaign.filter(camp => parseInt(camp.goal) >= 2000 && parseInt(camp.goal) < 10000)}
             else if(goalFilter === "10000-29999"){
-                draft.filteredCampaigns = draft.campaign.filter(camp => parseInt(camp.goal) >= 10000 && parseInt(camp.goal) < 30000);
-            }
+                draft.filteredCampaigns = draft.campaign.filter(camp => parseInt(camp.goal) >= 10000 && parseInt(camp.goal) < 30000)}
             else if(goalFilter === ">=30000"){
-                draft.filteredCampaigns = draft.campaign.filter(camp => parseInt(camp.goal) >= 30000);
-
-            }
+                draft.filteredCampaigns = draft.campaign.filter(camp => parseInt(camp.goal) >= 30000)}
             else{
-                draft.filteredCampaigns = draft.campaign;
-            }
+                draft.filteredCampaigns = draft.campaign}
 
+            // FILTER COUNTRIES
             if(countryFilter !== ""){
-                draft.filteredCampaigns = draft.filteredCampaigns.filter(camp => camp.location_country === countryFilter);
-
-            }
-
+                draft.filteredCampaigns = draft.filteredCampaigns.filter(camp => camp.location_country === countryFilter)}
+            
+            // FILTER STATES
             if(stateFilter !== ""){
-                draft.filteredCampaigns = draft.filteredCampaigns.filter(camp => camp.location_city.slice(-2) === stateFilter);
-            }   
+                draft.filteredCampaigns = draft.filteredCampaigns.filter(camp => camp.location_city.slice(-2) === stateFilter)}   
+                
+            // FILTER HAS_BENEFICIARY
+            if(hasBeneficiaryFilter === ""){}
+            else if(hasBeneficiaryFilter === "True"){
+                draft.filteredCampaigns = draft.filteredCampaigns.filter(camp => camp.has_beneficiary === "True")}
+            else if(hasBeneficiaryFilter === "False"){
+                draft.filteredCampaigns = draft.filteredCampaigns.filter(camp => camp.has_beneficiary === "False")}
+
+            // FILTER GOAL PERCENT COMPLETE
+            if(percentGoalFilter === ""){}
+            else if(percentGoalFilter === "<5"){
+                draft.filteredCampaigns = draft.filteredCampaigns.filter(camp => (parseFloat(camp.current_amount)/parseFloat(camp.goal)*100).toFixed(0) < 5)}
+            else if(percentGoalFilter === "5-15"){
+                draft.filteredCampaigns = draft.filteredCampaigns.filter(camp => (parseFloat(camp.current_amount)/parseFloat(camp.goal)*100).toFixed(0) >= 5 && (parseFloat(camp.current_amount)/parseFloat(camp.goal)*100).toFixed(0) < 15)}
+            else if(percentGoalFilter === "15-30"){
+                draft.filteredCampaigns = draft.filteredCampaigns.filter(camp => (parseFloat(camp.current_amount)/parseFloat(camp.goal)*100).toFixed(0) >= 15 && (parseFloat(camp.current_amount)/parseFloat(camp.goal)*100).toFixed(0) < 30)}
+            else if(percentGoalFilter === "30+"){
+                draft.filteredCampaigns = draft.filteredCampaigns.filter(camp => (parseFloat(camp.current_amount)/parseFloat(camp.goal)*100).toFixed(0) >= 30)}
             
         }))
     }
